3168e8c516006e1241ecf76a03d42306087efc10
Jordan Bravo <jordan@bravo.cc>
2023-09-29 16:19:56 -0400

    Lsp updates

--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -8,7 +8,7 @@ return {
   dependencies = {
     -- Useful status updates for LSP
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    { "j-hui/fidget.nvim",       tag = "legacy", opts = {} },
+    { "j-hui/fidget.nvim", tag = "legacy", opts = {} },
     "folke/neodev.nvim",
     "nvim-lua/plenary.nvim",
     "lukas-reineke/lsp-format.nvim",
@@ -17,12 +17,12 @@ return {
   },
   config = function()
     local lspconfig = require("lspconfig")
-    require("lsp-format").setup({}) 
+    require("lsp-format").setup({})
     require("neodev").setup()
 
     local on_attach = function(client, bufnr)
-        require("lsp-format").on_attach(client, bufnr)
-        -- ... custom code ...
+      require("lsp-format").on_attach(client, bufnr)
+      -- ... custom code ...
     end
 
 
@@ -31,11 +31,63 @@ return {
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-    lspconfig.tsserver.setup({ on_attach = on_attach })
+    -- JavaScript / TypeScript
+    lspconfig.tsserver.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
+    -- Lua
+    lspconfig.lua_ls.setup({
+      -- on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          workspace = { checkThirdParty = false }, -- make a note here of what this does
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          telemetry = { enable = false },
+          diagnostics = {
+            -- Get the language server to recognize the "vim" global variabe
+            globals = { "vim" },
+          },
+        },
+      },
+      -- Suggested setup for Neovim from nvim-lspconfig docs
+      -- on_init = function(client)
+      --   local path = client.workspace_folders[1].name
+      --   if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      --     client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+      --       Lua = {
+      --         -- Make the server aware of Neovim runtime files
+      --         workspace = {
+      --           checkThirdParty = false,
+      --           library = {
+      --             vim.env.VIMRUNTIME
+      --             -- "${3rd}/luv/library"
+      --             -- "${3rd}/busted/library",
+      --           }
+      --           -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+      --           -- library = vim.api.nvim_get_runtime_file("", true)
+      --         }
+      --       }
+      --     })
+      --
+      --     client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+      --   end
+      --   return true
+      -- end
+    })
     -- Python
-    lspconfig.pyright.setup({ on_attach = on_attach })
+    lspconfig.pyright.setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
     lspconfig.ruff_lsp.setup({
       on_attach = on_attach,
+      capabilities = capabilities,
       init_options = {
         settings = {
           -- Any extra CLI arguments for ruff go here.
@@ -43,9 +95,11 @@ return {
         }
       }
     })
+    -- Rust
     lspconfig.rust_analyzer.setup({
       -- Server-specific settings. See `:help lspconfig-setup`
       on_attach = on_attach,
+      capabilities = capabilities,
       cmd = { "rustup", "run", "stable", "rust-analyzer" },
       settings = {
         ['rust-analyzer'] = {
@@ -55,20 +109,6 @@ return {
         },
       },
     })
-
-    -- local servers = {
-    --   -- Lua
-    --   lua_ls = {
-    --     Lua = {
-    --       workspace = { checkThirdParty = false }, -- make a note here of what this does
-    --       telemetry = { enable = false },
-    --       diagnostics = {
-    --         -- Get the language server to recognize the "vim" global variabe
-    --         globals = { "vim" },
-    --       },
-    --     },
-    --   },
-    -- }
 
     -- local null_ls = require("null-ls")
     -- null_ls.setup({
