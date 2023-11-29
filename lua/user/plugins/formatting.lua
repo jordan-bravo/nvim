024e132af63c7d3646c1f64cae5635dece7a5bad
Jordan Bravo <jordan@bravo.cc>
2023-11-28 20:53:31 -0500

    disable formatting on save

--- lua/user/plugins/formatting.lua
+++ lua/user/plugins/formatting.lua
@@ -23,11 +23,11 @@ return {
         yaml = { { "prettierd", "prettier" } },
         markdown = { { "prettierd", "prettier" } },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      },
+      -- format_on_save = {
+      --   lsp_fallback = true,
+      --   async = false,
+      --   timeout_ms = 500,
+      -- },
     })
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
       conform.format({
