6b1a938209d9db1490dd839a2b8d4e3624a6738d
test-global <jordan@bravo.cc>
2024-04-23 20:29:38 -0400

    add new dir structure

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 -- nvim/init.lua
 
-require("user.options")
-require("user.plugins")
-require("user.lazy")
-require("user.keymaps")
+require("jordan.options")
+-- require("jordan.keymaps")
+-- require("jordan.lazy")
+
+-- old
+-- require("user.options")
+-- require("user.plugins")
+-- require("user.lazy")
+-- require("user.keymaps")
