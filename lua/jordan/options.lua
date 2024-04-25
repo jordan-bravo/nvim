11eb1abf4296041485c3e1d15cfbd61e30d142f6
Jordan Bravo <jordan@bravo.cc>
2024-04-24 20:20:15 -0400

    start refactor

---
+++ lua/jordan/options.lua
@@ -0,0 +1,23 @@
+-- nvim/lua/jordan/options.lua
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a' -- enables mouse
+vim.opt.showmode = false -- already shown in status line
+vim.opt.breakindent = true -- wrapped lines are visually indented
+vim.opt.undofile = true -- save undo history
+vim.opt.ignorecase = true -- case insensitive search
+vim.opt.smartcase = true -- case sensitive search if search contains uppercase
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250 -- decrease update time for swap file autosave and CursorHold
+vim.opt.timeoutlen = 300 -- mapped sequence wait time, e.g. which-key
+vim.opt.splitright = true -- default vertical split to the right
+vim.opt.splitbelow = true -- default split to the bottom
+vim.opt.list = true -- sets how to display certain whitespace chars
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.inccommand = 'split' -- live substitution preview while typing
+vim.opt.cursorline = true -- show which line the cursor is on
+vim.opt.scrolloff = 10 -- minimum lines to keep above and below the cursor
