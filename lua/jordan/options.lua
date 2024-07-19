0a78ad11012f50f92f86d1229cb7f0fbeff3c01d
Jordan Bravo <jordan@bravo.cc>
2024-07-19 13:26:27 -0400

    disable netrw

--- lua/jordan/options.lua
+++ lua/jordan/options.lua
@@ -2,6 +2,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
+vim.g.loaded_netrw = 1 -- disable netrw
+vim.g.loaded_netrwPlugin = 1 -- repl
 
 vim.opt.number = true
 vim.opt.relativenumber = true
--- user/options.lua
+++ user/options.lua
@@ -7,12 +7,12 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- vim.g.mapleader = ' '
+-- vim.g.maplocalleader = ' '
 
 -- Disable netrw and replace with a plugin
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 -- Set highlight on search
 vim.o.hlsearch = false
