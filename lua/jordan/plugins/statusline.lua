0d8041b49e1fe6d60d8ae90b6badce11a01d20f1
Jordan Bravo <jordan@bravo.cc>
2024-08-30 12:36:19 -0400

    make statusline show file path

--- lua/jordan/plugins/statusline.lua
+++ lua/jordan/plugins/statusline.lua
@@ -3,9 +3,20 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup({
-      --options = {
-      --  theme = "vscode",
-      --},
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            -- path options:
+            -- 0: Just the filename
+            -- 1: Relative path
+            -- 2: Absolute path
+            -- 3: Absolute path, with tilde as the home directory
+            -- 4: Filename and parent dir, with tilde as the home directory
+            path = 1,
+          },
+        },
+      },
     })
   end,
 }
