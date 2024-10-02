8af83dd7966d9b050511ba4dea4a990b361532d0
Jordan Bravo <jordan@bravo.cc>
2024-10-02 16:54:46 -0400

    change scroll offset from 10 to 0

--- lua/jordan/options.lua
+++ lua/jordan/options.lua
@@ -24,7 +24,7 @@ vim.opt.list = true -- sets how to display certain whitespace chars. see next li
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split' -- live substitution preview while typing
 vim.opt.cursorline = true -- show which line the cursor is on
-vim.opt.scrolloff = 10 -- minimum lines to keep above and below the cursor
+vim.opt.scrolloff = 0 -- minimum lines to keep above and below the cursor
 vim.opt.tabstop = 2 -- default tabstop if no filetype match
 vim.opt.shiftwidth = 0 -- set shiftwidth to same value as tabstop
 vim.opt.expandtab = true -- tabs insert spaces
