82494340e0a8fbabf58813c4b52f30171ba08ac1
Jordan Bravo <jordan@bravo.cc>
2024-10-08 13:37:15 -0400

    keymap: <leader>a -> show diagnostic float

--- lua/jordan/keymaps.lua
+++ lua/jordan/keymaps.lua
@@ -6,7 +6,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>a', vim.diagnostic.open_float, { desc = 'Show di[a]gnostic Error messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Toggle diagnostic virtual text
