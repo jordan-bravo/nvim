237eea9d9a2ca512e12b75946e97941724fca89d
Jordan Bravo <jordan@bravo.cc>
2026-05-06 18:41:21 +0000

    update lspconfig api

--- lua/jordan/plugins/lsp.lua
+++ lua/jordan/plugins/lsp.lua
@@ -7,15 +7,9 @@ return {
       { 'antosha417/nvim-lsp-file-operations', config = true },
     },
     config = function()
-      -- import lspconfig plugin
-      local lspconfig = require('lspconfig')
-
-      -- import cmp-nvim-lsp plugin
       local cmp_nvim_lsp = require('cmp_nvim_lsp')
-
       local on_attach = require('jordan.keymaps').on_attach
 
-      -- used to enable autocompletion (assign to every lsp server config)
       local capabilities = cmp_nvim_lsp.default_capabilities()
 
       -- Languages that use the vscode-langservers-extracted (vle) language server
@@ -24,35 +18,25 @@ return {
       vle_capabilities.textDocument.completion.completionItem.snippetSupport = true
 
       -- Change the Diagnostic symbols in the sign column (gutter)
-      local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
+      local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
       for type, icon in pairs(signs) do
         local hl = 'DiagnosticSign' .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
       end
 
-      -- configure c & c++ language server
-      lspconfig['ccls'].setup({
+      vim.lsp.config('*', {
         capabilities = capabilities,
         on_attach = on_attach,
       })
 
-      -- configure css server
-      lspconfig['cssls'].setup({
-        capabilities = vle_capabilities,
-        on_attach = on_attach,
-      })
+      vim.lsp.config('cssls', { capabilities = vle_capabilities })
+      vim.lsp.config('html', { capabilities = vle_capabilities })
 
-      -- configure emmet language server
-      lspconfig['emmet_ls'].setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+      vim.lsp.config('emmet_ls', {
         filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'svelte' },
       })
 
-      -- configure golang servers
-      lspconfig['gopls'].setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+      vim.lsp.config('gopls', {
         settings = {
           gopls = {
             gofumpt = true,
@@ -66,25 +50,9 @@ return {
         },
       })
 
-      -- configure graphql language server
-      -- lspconfig["graphql"].setup({
-      --   capabilities = capabilities,
-      --   on_attach = on_attach,
-      --   filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-      -- })
-
-      -- configure html server
-      lspconfig['html'].setup({
-        capabilities = vle_capabilities,
-        on_attach = on_attach,
-      })
-
-      -- configure lua server (with special settings)
-      lspconfig['lua_ls'].setup({
-        -- TODO: Research the plugin neodev, which helps editing nvim specific lua files
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = { -- custom settings for lua
+      -- TODO: Research the plugin neodev, which helps editing nvim specific lua files
+      vim.lsp.config('lua_ls', {
+        settings = {
           Lua = {
             -- make the language server recognize "vim" global
             diagnostics = {
@@ -101,13 +69,7 @@ return {
         },
       })
 
-      -- -- nil_ls
-      lspconfig['nil_ls'].setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        autostart = true,
-        -- capabilities = caps,
-        -- cmd = { lsp_path },
+      vim.lsp.config('nil_ls', {
         settings = {
           ['nil'] = {
             formatting = {
@@ -117,21 +79,7 @@ return {
         },
       })
 
-      -- configure prisma orm server
-      -- lspconfig["prismals"].setup({
-      --   capabilities = capabilities,
-      --   on_attach = on_attach,
-      -- })
-
-      -- configure python servers
-      -- lspconfig['basedpyright'].setup({
-      lspconfig['pyright'].setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
-      lspconfig['ruff'].setup({
-        -- capabilities = capabilities,
-        on_attach = on_attach,
+      vim.lsp.config('ruff', {
         init_options = {
           settings = {
             -- Any extra CLI arguments for `ruff` go here.
@@ -139,58 +87,21 @@ return {
           },
         },
       })
-      -- configure rust server
-      lspconfig['rust_analyzer'].setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
 
-      -- configure svelte server
-      -- lspconfig["svelte"].setup({
-      --   capabilities = capabilities,
-      --   on_attach = function(client, bufnr)
-      --     on_attach(client, bufnr)
-      --
-      --     vim.api.nvim_create_autocmd("BufWritePost", {
-      --       pattern = { "*.js", "*.ts" },
-      --       callback = function(ctx)
-      --         if client.name == "svelte" then
-      --           client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
-      --         end
-      --       end,
-      --     })
-      --   end,
-      -- })
-
-      -- configure tailwindcss server
-      lspconfig['tailwindcss'].setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
-
-      -- configure typescript server with plugin
-      lspconfig['ts_ls'].setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+      vim.lsp.enable({
+        'ccls',
+        'cssls',
+        'emmet_ls',
+        'gopls',
+        'html',
+        'lua_ls',
+        'nil_ls',
+        'pyright',
+        'ruff',
+        'rust_analyzer',
+        'tailwindcss',
+        'ts_ls',
       })
     end,
   },
-  -- {
-  --   'WhoIsSethDaniel/toggle-lsp-diagnostics.nvim',
-  --   config = function()
-  --     require('toggle_lsp_diagnostics').init({
-  --       start_on = true,
-  --     })
-  --   end,
-  -- },
-  -- lsp lines looks cool but there's a problem, where it's unclear how it
-  -- interacts with virtual text. Needs further investigation to get the right
-  -- combo of starting settings (on/off) and keymaps for the interaction between
-  -- virtual text and virtual lines. For now, disabling virtual lines.
-  -- {
-  --   'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-  --   config = function()
-  --     require('lsp_lines').setup()
-  --   end,
-  -- },
 }
